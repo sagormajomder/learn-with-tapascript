df5c9209aa563bcfbe2e8ff0b1d1b99be3400adf
Sagor Majomder <sagormajomder@gmail.com>
2025-05-17 16:10:38 +0600

    question added to day 04

--- 40daysJS/Day 04/task/script.js
+++ 40daysJS/Day 04/task/script.js
@@ -104,6 +104,7 @@ const side1 = 5,
 console.log(side1, side2, side3);
 
 if (side1 === side2 && side1 === side3) console.log('Equilateral Triangle');
-else if (side1 === side2 || side1 === side3) console.log('Isosceles Triangle');
+else if (side1 === side2 || side2 === side3 || side1 === side3)
+  console.log('Isosceles Triangle');
 else console.log('Scalene Triangle');
 console.log('');
